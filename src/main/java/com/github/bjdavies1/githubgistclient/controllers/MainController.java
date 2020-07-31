693c64ed1e7368d0a7532c2f6016c4a97ff3f891
Brady Davies <stargatefan.bd@gmail.com>
2020-07-30 20:36:07 -0600

    Began to add more funtionality

--- src/main/java/com/github/bjdavies1/githubgistclient/controllers/MainController.java
+++ src/main/java/com/github/bjdavies1/githubgistclient/controllers/MainController.java
@@ -1,4 +1,16 @@
 package com.github.bjdavies1.githubgistclient.controllers;
 
-public class MainController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.net.http.HttpClient;
+
+public class MainController implements Initializable {
+
+    private HttpClient client;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        client = HttpClient.newHttpClient();
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module GitHubSimpleInfoClient {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+    requires java.net.http;
+    requires com.google.gson;
 
     opens com.github.bjdavies1.githubgistclient;
     opens com.github.bjdavies1.githubgistclient.controllers;
